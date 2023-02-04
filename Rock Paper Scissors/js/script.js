e4d5d8d255c07d0d1391eae156959e6c381cdade
kimminam <bluemind8520@gmail.com>
2023-02-05 03:20:57 +0900

    [feat] DarkMode 완성

--- Rock Paper Scissors/js/script.js
+++ Rock Paper Scissors/js/script.js
@@ -1,6 +1,8 @@
 const DarkMode = document.querySelector('#Dark');
 const Main = document.querySelector('.maincontainer');
 const All = document.querySelector('*');
+const Score = document.querySelectorAll('.ScoreBorad');
+console.log(Score)
 function Dark_mode() {
     const checkbox = document.querySelector('#Dark');
     const is_checked = checkbox.checked;
@@ -9,7 +11,11 @@ function Dark_mode() {
         Main.style.backgroundColor = '#333';
         DarkMode.style.backgroundColor = "#eee";
         DarkMode.style.color = "#000";
-        All.style.color = "#ddd"
+        All.style.color = "#ddd";
+        for(let i = 0; i<Score.length; i++) {
+            Score[i].style.backgroundColor = "#fff";
+            Score[i].style.color = "#000";
+        }
     }
     else {
         Main.style.backgroundColor = '#fff';
